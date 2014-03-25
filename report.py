02b13b173b4a00a7d574ff6d3ef9664395da2919
A. Bram Neijt <bneijt@gmail.com>
2014-03-25 21:05:05 +0100

    Add report script to generate HTML

---
+++ report.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+# Bucketcam: limited best effort image storage
+# Copyright (C) 2024 Bram Neijt
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import store
+import logging
+import os.path
+
+logging.basicConfig(level = logging.DEBUG, format = '%(asctime)s %(levelname)s %(message)s')
+logger = logging.getLogger(__name__)
+
+hashCache = {}
+HTML_HEAD = '''
+<html>
+<head><style>
+figure {
+    max-width: 200px;
+    display: inline-block;
+    text-align: center;
+}
+figure.approximateImage {
+    background-color: silver;
+    color: black;
+    font-style: italic
+}
+img {
+    max-width: 100%;
+}
+</style></head>
+<body>
+'''
+HTML_TAIL='''
+</body></html>
+'''
+
+def currentHashOf(filename):
+    if not filename in hashCache:
+        hashCache[filename] = store.hashOfFile(filename)
+    return hashCache[filename]
+
+def logFiles():
+    for entry in os.listdir("images"):
+        if entry.startswith("storage_") and entry.endswith(".log"):
+            yield os.path.join("images", entry)
+def imageTag(timestamp, filename, exactFile = False):
+    return '''
+<figure class="%(figureClass)s">
+    <a href="%(src)s">
+        <img src="%(src)s" alt="Image from %(timestamp)s"/>
+    </a>
+    <figcaption>%(timestamp)s</figcaption>
+</figure>''' % {
+        "src": filename,
+        "timestamp": timestamp,
+        "figureClass": ("exactImage" if exactFile else "approximateImage")
+    }
+
+def main():
+    for logFilename in logFiles():
+        htmlFilename = os.path.splitext(logFilename)[0] + ".html"
+        logger.info("Processing %s -> %s", logFilename, htmlFilename)
+        with open(logFilename, 'r') as logFile:
+            with open(htmlFilename, 'w') as htmlFile:
+                htmlFile.write(HTML_HEAD)
+                for line in logFile.readlines():
+                    line = line.strip()
+                    (timestamp, hashWhenStored, filename) = line.split(" ", 2)
+                    fileStillExists = os.path.exists(filename) 
+                    currentHash = currentHashOf(filename) if fileStillExists else "?"
+                    if not fileStillExists:
+                        filename = "images/http://placehold.it/200x200&text=404"
+                    htmlFile.write(imageTag(timestamp, filename[len("images/"):], exactFile = hashWhenStored == currentHash))
+                htmlFile.write(HTML_TAIL)
+
+if __name__ == "__main__":
+    main()
