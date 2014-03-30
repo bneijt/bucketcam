44a6fa0d6f0e86d7621b0db9f75805fedb865ce9
A. Bram Neijt <bneijt@gmail.com>
2014-03-30 20:55:04 +0200

    Add flask to the mix

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
+cd "`dirname "$0"`"
 if [ ! -d "virtualenv" ]; then
     virtualenv -p `which python3` virtualenv
 fi
 
 . virtualenv/bin/activate
-pip install pillow
-pip install arrow
+pip install pillow==2.3.1
+pip install arrow==0.4.2
+pip install flask==0.10.1
 echo 'You need virtualenv to work with this'
 echo '. virtualenv/bin/activate'
 
