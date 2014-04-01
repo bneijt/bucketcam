9108b8ea455955a59f43f5c649659c407aa916ce
A. Bram Neijt <bneijt@gmail.com>
2014-04-01 23:01:44 +0200

    Use FFT as levels of detail

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@ fi
 . virtualenv/bin/activate
 pip install pillow==2.3.1
 pip install arrow==0.4.2
-pip install flask==0.10.1
+pip install numpy==1.8.1
 echo 'You need virtualenv to work with this'
 echo '. virtualenv/bin/activate'
 
--- store.py
+++ store.py
@@ -15,7 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 from PIL import Image, ImageFilter
 import io
 import logging
@@ -28,6 +27,8 @@ import shutil
 import hashlib
 import random
 import arrow
+import numpy.fft
+import math
 
 logging.basicConfig(level = logging.INFO, format = '%(asctime)s %(levelname)s %(message)s')
 logger = logging.getLogger(__name__)
@@ -50,6 +51,7 @@ class ImageLoader(object):
         while True:
             yield self.loadCameraImage(self.config)
     def loadCameraImage(self, config):
+        # return Image.open("original.png")
         url = config.get("source", "imageDownloadUrl")
 
         password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
@@ -64,52 +66,6 @@ class ImageLoader(object):
         r = opener.open(url)
         imageData = io.BytesIO(r.read())
         return Image.open(imageData)
-
-def meanValue(image):
-    #General value of image
-    return image.convert("L").resize((1,1)).getpixel((0,0))
-
-def countRed(image):
-    #Number of colors used in image
-    assert image.mode == "RGB"
-    data = image.resize((100,100)).getdata(band = 0)
-    return len(set(data))
-
-def countGreen(image):
-    '''Different values of green
-    used in the image'''
-    assert image.mode == "RGB"
-    data = image.resize((100,100)).getdata(band = 1)
-    return len(set(data))
-
-def countBlue(image):
-    '''Different values of blue
-    used in the image'''
-    assert image.mode == "RGB"
-    data = image.resize((100,100)).getdata(band = 2)
-    return len(set(data))
-
-def binaryPixelCount(image):
-    '''Number of pixels that fall in to the first of two categories
-    when the image has been converted to two colors'''
-    return image.convert("L").quantize(2).histogram()[0]
-
-
-def edgeValueForQuadrant(quadrantIndex):
-    def q(image):
-        edges = image.filter(ImageFilter.FIND_EDGES).convert("L")
-        w, h = image.size
-        quadrants = (
-          (0, 0, w // 2, h // 2),
-          (w // 2, 0, w, h // 2),
-          (0, h // 2, w // 2, h),
-          (w // 2, h // 2, w, h)
-         )
-        return edges.crop(quadrants[quadrantIndex]).resize((1,1)).getpixel((0,0))
-    return q
-
-def randomValue(image):
-    return random.randint(0, 1000)
 
 
 def logStorage(filename):
@@ -128,18 +84,6 @@ def readLogStorage(filename):
                 "hash": hashWhenStored,
                 "filename": filename.strip()
             }
-
-LEVELS_OF_DETAIL = [
-    meanValue,
-    countRed,
-    countGreen,
-    countBlue,
-    edgeValueForQuadrant(3),
-    edgeValueForQuadrant(2),
-    edgeValueForQuadrant(1),
-    edgeValueForQuadrant(0),
-    binaryPixelCount
-]
 
 class StorageLimit(object):
     '''Simple storage limit counter'''
@@ -162,24 +106,25 @@ class StorageLimit(object):
         return self._useCount
 
 class LevelOfDetail(object):
-    def __init__(self, levels):
-        assert isinstance(levels, list)
-        assert len(levels) >= 1
-        logger.debug("LOD with levels %s", repr(levels))
-        self.levels =  levels
+    def __init__(self, image, desiredLevelCount):
+        assert hasattr(image, "convert")
+        #Calculate the resize needed to get around the desiredLevelCount
+        size = math.ceil(math.sqrt(desiredLevelCount))
+        levels =  [abs(i) for i in numpy.fft.rfft( image.convert("L").resize((size, size)).getdata() )]
+        levels = levels[1:] #Throw away the first level (mean intensity)
+        logger.debug("FFT resulted in %i levels", len(levels))
+        maxEntriesPerDirectory = 1024
+        normalizationFactor = maxEntriesPerDirectory / max(levels)
+        self.levels = [int(round((normalizationFactor * i))) for i in levels]
+        logger.debug("Done calculating FFT")
 
-    @staticmethod
-    def fromImageAtLevel(image, toplevel):
-        levels = []
-        for l in range(toplevel + 1):
-            levels.append(LevelOfDetail.imageTagAtLevel(image, l))
-        return LevelOfDetail(levels)
+    def toLevel(self, level):
+        assert level >= 0
+        assert level < len(self.levels)
+        self.level = level
 
-    @staticmethod
-    def imageTagAtLevel(image, level):
-        assert level < len(LEVELS_OF_DETAIL)
-        v = str(LEVELS_OF_DETAIL[level](image))
-        return v
+    def currentLevels(self):
+        return self.levels[:self.level + 1]
 
     def remove(self, storageLimit):
         #Remove anything already there
@@ -206,7 +151,7 @@ class LevelOfDetail(object):
         return removeCount
 
     def getLevel(self):
-        return len(self.levels) -1
+        return self.level
 
     def isOccupied(self):
         loc = self.path()
@@ -228,6 +173,9 @@ class LevelOfDetail(object):
         storageLimit.inc(1)
         logStorage(loc)
 
+    def hasMoreLevels(self):
+        return self.level < len(self.levels)
+
     def branch(self, storageLimit):
         '''Branch at the current level
 
@@ -235,20 +183,15 @@ class LevelOfDetail(object):
         We have to remove the image because the change of removing
         the branch in the future is minimal because the main function
         aggresively enters branches on collisions.
-
-        If the current level of detail is the maximum level of detail,
-        branching will fail and return False. It returns true otherwise.
         '''
-        if len(self.levels) >= len(LEVELS_OF_DETAIL):
-            return False
         self.remove(storageLimit)
         loc = self.path()
         if not os.path.exists(loc):
             os.makedirs(loc)
-        return True
+
 
     def path(self):
-        return os.path.join("images", *self.levels)
+        return os.path.join("images", *[ str(l) for l in self.currentLevels()])
 
 
 def loadConfig():
@@ -261,9 +204,11 @@ def loadConfig():
 def loadAndStoreImage(image, storageLimit):
     logger.info("Storage limit at %i out of %i" % storageLimit.usedAndLimit())
     #Load image
-    lod = LevelOfDetail.fromImageAtLevel(image, 0)
-    while lod.hasBranched():
-        lod = LevelOfDetail.fromImageAtLevel(image, lod.getLevel() + 1)
+    desiredLevelCount = 30
+    lod = LevelOfDetail(image, desiredLevelCount)
+    lod.toLevel(0)
+    while lod.hasBranched() and lod.hasMoreLevels():
+        lod.toLevel(lod.getLevel() + 1)
 
 
 
@@ -273,7 +218,7 @@ def loadAndStoreImage(image, storageLimit):
         # We need to start pruning this branch
         if lod.getLevel() > 0:
             logger.warning("Moving down one level of detail to save storage space")
-            lod = LevelOfDetail.fromImageAtLevel(image, lod.getLevel() - 1)
+            lod.toLevel(lod.getLevel() - 1)
         elif not lod.isOccupied():
             logger.warning("Should be doing some kind of level 0 random pruning")
             logger.error("Probably exceeding limit because of excess storage in level 0")
@@ -281,10 +226,12 @@ def loadAndStoreImage(image, storageLimit):
         lod.store(image, storageLimit)
     else:
         if lod.isOccupied():
-            #Just branch if there is a collision
-            if lod.branch(storageLimit):
-                logger.debug("Branching into level %i", lod.getLevel() + 1)
-                lod = LevelOfDetail.fromImageAtLevel(image, lod.getLevel() + 1)
+            #Just branch if there is a collision and we have more levels
+            # because storage limit has not been reached yet
+            if lod.hasMoreLevels():
+                lod.branch(storageLimit)
+                logger.debug("Branching succeeded into level %i", lod.getLevel() + 1)
+                lod.toLevel(lod.getLevel() + 1)
             else:
                 logger.warn("More images allowed, but no more levels of detail left")
         lod.store(image, storageLimit)
